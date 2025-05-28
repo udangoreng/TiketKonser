5f3d80cac61cc4e98476fa83815320c2b7872212
hasnatya <hasnatya22@gmail.com>
2025-05-28 16:06:32 +0700

    Add LoginInterface

---
+++ src/tiketKonser/DetailKonserInterface.java
@@ -0,0 +1,161 @@
+package tiketKonser;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class DetailKonserInterface extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DetailKonserInterface frame = new DetailKonserInterface();
+					frame.setVisible(true);
+					frame.setSize(1024, 678);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public DetailKonserInterface() {
+		setBackground(new Color(81, 20, 101));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1224, 690);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(26, 21, 24));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(81, 20, 101));
+		panel.setBounds(0, 0, 255, 661);
+		panel.setSize(250,678);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Hi, Username");
+		lblNewLabel.setBounds(44, 51, 149, 22);
+		panel.add(lblNewLabel);
+		lblNewLabel.setFont(new Font("Poppins", Font.BOLD, 20));
+		lblNewLabel.setBackground(new Color(26, 21, 24));
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		
+		JLabel lblNewLabel_1 = new JLabel("Lihat Konser");
+		lblNewLabel_1.setFont(new Font("Poppins Medium", Font.PLAIN, 14));
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setBackground(new Color(26, 21, 24));
+		lblNewLabel_1.setBounds(44, 200, 153, 31);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Tiket Saya");
+		lblNewLabel_2.setBackground(new Color(26, 21, 24));
+		lblNewLabel_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_2.setFont(new Font("Poppins Medium", Font.PLAIN, 14));
+		lblNewLabel_2.setBounds(44, 242, 138, 22);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Profil Saya");
+		lblNewLabel_3.setFont(new Font("Poppins Medium", Font.PLAIN, 14));
+		lblNewLabel_3.setBackground(new Color(26, 21, 24));
+		lblNewLabel_3.setForeground(new Color(255, 255, 255));
+		lblNewLabel_3.setBounds(44, 275, 133, 31);
+		panel.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("LogOut");
+		lblNewLabel_4.setFont(new Font("Poppins Medium", Font.PLAIN, 14));
+		lblNewLabel_4.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4.setBackground(new Color(26, 21, 24));
+		lblNewLabel_4.setBounds(44, 593, 79, 22);
+		panel.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("BRUNO MARS - JAKARTA INTERNASIONAL  STADIUM");
+		lblNewLabel_5.setFont(new Font("Poppins", Font.BOLD, 20));
+		lblNewLabel_5.setForeground(new Color(255, 255, 255));
+		lblNewLabel_5.setBounds(272, 27, 554, 62);
+		contentPane.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("");
+		lblNewLabel_6.setBounds(271, 88, 350, 450);
+
+		ImageIcon icon = new ImageIcon("C:\\Users\\hasnatyam\\Downloads\\download.jpg");
+		Image img = icon.getImage();
+		Image newImg = img.getScaledInstance(350, 450, Image.SCALE_SMOOTH);
+		lblNewLabel_6.setIcon(new ImageIcon(newImg));
+
+		contentPane.add(lblNewLabel_6);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(647, 88, 478, 372);
+		textArea.setEditable(false);
+		textArea.setWrapStyleWord(true);
+		textArea.setLineWrap(true);
+		textArea.setFont(new Font("Poppins", Font.PLAIN, 13));
+		textArea.setForeground(Color.WHITE);
+		textArea.setBackground(new Color(26, 21, 24));
+		textArea.setText(
+		    "🎤 Konser Bruno Mars - Jakarta 2024\n\n" +
+		    "📅 Tanggal: 13-14 September 2024\n" +
+		    "🕗 Waktu: 20:00 WIB (pintu dibuka 18:00 WIB)\n" +
+		    "📍 Lokasi:\n" +
+		    "Jakarta International Stadium (JIS)\n" +
+		    "Jl. Sunter Permai Raya, Tanjung Priok, Jakarta Utara\n\n" +
+		    "👨‍🎤 Artis:\n" +
+		    "Bruno Mars & The Hooligans\n\n" +
+		    "Tentang Artis\n"+
+		    "Bruno Mars (nama asli: Peter Gene Hernandez) adalah penyanyi, penulis lagu, dan produser musik asal Amerika Serikat yang dikenal dengan gaya retro dan funky. Kariernya melejit sejak 2010 dan ia dikenal sebagai salah satu performer terbaik secara live.\n\n"+
+		    "Genre: Pop, Funk, R&B\n" +
+		    "Hits: Uptown Funk, 24K Magic, Just The Way You Are, When I Was Your Man\n\n"
+		);
+		contentPane.add(textArea);
+		
+		JButton btnNewButton = new JButton("pesan tiket");
+		btnNewButton.setFont(new Font("Poppins", Font.BOLD, 13));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.setBounds(1020, 515, 142, 23);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel_7 = new JLabel("Kategori seat:");
+		lblNewLabel_7.setBackground(new Color(26, 21, 24));
+		lblNewLabel_7.setFont(new Font("Poppins", Font.PLAIN, 13));
+		lblNewLabel_7.setForeground(new Color(255, 255, 255));
+		lblNewLabel_7.setBounds(647, 473, 98, 14);
+		contentPane.add(lblNewLabel_7);
+		
+		JLabel lblNewLabel_8 = new JLabel("klick for more info");
+		lblNewLabel_8.setFont(new Font("Poppins", Font.PLAIN, 13));
+		lblNewLabel_8.setForeground(new Color(0, 128, 255));
+		lblNewLabel_8.setBackground(new Color(26, 21, 24));
+		lblNewLabel_8.setBounds(742, 473, 137, 14);
+		contentPane.add(lblNewLabel_8);
+	}
+}
--- src/tiketKonser/LoginInterface.java
+++ src/tiketKonser/LoginInterface.java
@@ -12,10 +12,14 @@ import javax.swing.JTextPane;
 import javax.swing.JTextField;
 import java.awt.Font;
 import javax.swing.JCheckBox;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JLayeredPane;
+import javax.swing.SwingConstants;
 
 public class LoginInterface extends JFrame {
 
@@ -45,92 +49,116 @@ public class LoginInterface extends JFrame {
 	 */
 	public LoginInterface() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1029, 759);
+		setBounds(100, 100, 1171, 737);
 		contentPane = new JPanel();
+		contentPane.setForeground(Color.WHITE);
+		contentPane.setBackground(new Color(26, 21, 24));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(640, 345, 218, 27);
+		passwordField.setBounds(731, 344, 218, 27);
 		contentPane.add(passwordField);
 		
 		JTextPane textPane = new JTextPane();
-		textPane.setBounds(640, 293, 218, 27);
+		textPane.setBounds(731, 293, 218, 27);
+		textPane.setBorder(BorderFactory.createLineBorder(getForeground()));
 		contentPane.add(textPane);
 		
 		JLabel lblNewLabel = new JLabel("Email :");
-		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 15));
-		lblNewLabel.setBounds(570, 293, 129, 27);
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setFont(new Font("Poppins", Font.BOLD, 15));
+		lblNewLabel.setBounds(664, 293, 129, 27);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Password :");
-		lblNewLabel_1.setFont(new Font("Verdana", Font.BOLD, 15));
-		lblNewLabel_1.setBounds(539, 343, 118, 27);
+		lblNewLabel_1.setForeground(Color.WHITE);
+		lblNewLabel_1.setFont(new Font("Poppins", Font.BOLD, 15));
+		lblNewLabel_1.setBounds(632, 343, 118, 27);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("LogIn");
-		lblNewLabel_2.setFont(new Font("Verdana", Font.BOLD, 20));
-		lblNewLabel_2.setBounds(691, 235, 136, 53);
+		lblNewLabel_2.setForeground(Color.WHITE);
+		lblNewLabel_2.setFont(new Font("Poppins", Font.BOLD, 20));
+		lblNewLabel_2.setBounds(794, 227, 136, 53);
 		contentPane.add(lblNewLabel_2);
 		
 		JCheckBox chckbxNewCheckBox = new JCheckBox("Remember me");
-		chckbxNewCheckBox.setBounds(639, 398, 219, 27);
+		chckbxNewCheckBox.setBackground(new Color(26, 21, 24));
+		chckbxNewCheckBox.setForeground(new Color(255, 255, 255));
+		chckbxNewCheckBox.setFont(new Font("Poppins", Font.PLAIN, 11));
+		chckbxNewCheckBox.setBounds(730, 399, 219, 27);
 		contentPane.add(chckbxNewCheckBox);
 		
 		JButton btnNewButton = new JButton("LogIn");
-		btnNewButton.setFont(new Font("Verdana", Font.BOLD, 15));
+		btnNewButton.setFont(new Font("Poppins", Font.BOLD, 15));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(640, 417, 218, 41);
+		btnNewButton.setBounds(731, 418, 218, 41);
 		contentPane.add(btnNewButton);
 		
 		JLabel lblNewLabel_3 = new JLabel("forgot password");
-		lblNewLabel_3.setFont(new Font("Verdana", Font.PLAIN, 12));
-		lblNewLabel_3.setBounds(752, 377, 173, 14);
+		lblNewLabel_3.setForeground(Color.WHITE);
+		lblNewLabel_3.setFont(new Font("Poppins", Font.PLAIN, 12));
+		lblNewLabel_3.setBounds(851, 377, 173, 14);
 		contentPane.add(lblNewLabel_3);
 		
 		JLabel lblNewLabel_4 = new JLabel("Don't have account?");
-		lblNewLabel_4.setForeground(new Color(0, 0, 0));
-		lblNewLabel_4.setFont(new Font("Verdana", Font.PLAIN, 12));
-		lblNewLabel_4.setBounds(640, 469, 201, 14);
+		lblNewLabel_4.setForeground(Color.WHITE);
+		lblNewLabel_4.setFont(new Font("Poppins", Font.PLAIN, 12));
+		lblNewLabel_4.setBounds(741, 469, 136, 14);
 		contentPane.add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_5 = new JLabel("SignUp");
 		lblNewLabel_5.setForeground(new Color(0, 128, 255));
-		lblNewLabel_5.setFont(new Font("Verdana", Font.PLAIN, 12));
-		lblNewLabel_5.setBounds(778, 469, 49, 14);
+		lblNewLabel_5.setFont(new Font("Poppins", Font.PLAIN, 12));
+		lblNewLabel_5.setBounds(870, 470, 49, 14);
 		contentPane.add(lblNewLabel_5);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(128, 128, 192));
-		panel.setBounds(-22, 0, 510, 849);
-		contentPane.add(panel);
+		panel.setBackground(new Color(81, 20, 101));
+		panel.setBounds(0, 0, 510, 849);
 		panel.setLayout(null);
+		contentPane.add(panel);
+
+		JLabel lblNewLabel_10 = new JLabel();
+		lblNewLabel_10.setBounds(0, 0, 510, 704);
+		lblNewLabel_10.setIcon(new ImageIcon("C:\\Users\\hasnatyam\\Downloads\\download (1).jpg"));
 		
-		JLabel lblNewLabel_6 = new JLabel("StagePass");
-		lblNewLabel_6.setForeground(new Color(255, 255, 255));
-		lblNewLabel_6.setBounds(213, 215, 255, 44);
-		panel.add(lblNewLabel_6);
-		lblNewLabel_6.setFont(new Font("Verdana", Font.BOLD | Font.ITALIC, 30));
+		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane.setBounds(0, 0, 510, 704);
+		layeredPane.setLayout(null);
+		panel.add(layeredPane);
+
 		
+		layeredPane.add(lblNewLabel_10, Integer.valueOf(0));
+
 		JLabel lblNewLabel_7 = new JLabel("WELCOME TO ");
-		lblNewLabel_7.setFont(new Font("Verdana", Font.BOLD | Font.ITALIC, 30));
-		lblNewLabel_7.setForeground(new Color(255, 255, 255));
-		lblNewLabel_7.setBounds(140, 125, 246, 122);
-		panel.add(lblNewLabel_7);
-		
+		lblNewLabel_7.setBounds(30, 88, 400, 50); 
+		lblNewLabel_7.setFont(new Font("Poppins", Font.BOLD, 30));
+		lblNewLabel_7.setForeground(new Color(0, 0, 0));
+		layeredPane.add(lblNewLabel_7, Integer.valueOf(1));
+
+		JLabel lblNewLabel_6 = new JLabel("StagePass");
+		lblNewLabel_6.setBounds(202, 121, 400, 44);
+		lblNewLabel_6.setFont(new Font("Poppins", Font.BOLD | Font.ITALIC, 30));
+		lblNewLabel_6.setForeground(new Color(0, 0, 0));
+		layeredPane.add(lblNewLabel_6, Integer.valueOf(1));
+
 		JLabel lblNewLabel_8 = new JLabel("Dari Layar ke Panggung");
-		lblNewLabel_8.setFont(new Font("Verdana", Font.BOLD, 20));
-		lblNewLabel_8.setBounds(121, 305, 318, 26);
-		panel.add(lblNewLabel_8);
-		
+		lblNewLabel_8.setBounds(30, 206, 400, 26);
+		lblNewLabel_8.setFont(new Font("Poppins", Font.BOLD, 20));
+		lblNewLabel_8.setForeground(new Color(0, 0, 0));
+		layeredPane.add(lblNewLabel_8, Integer.valueOf(1));
+
 		JLabel lblNewLabel_9 = new JLabel("Semua Dimulai di Stagepass");
-		lblNewLabel_9.setFont(new Font("Verdana", Font.BOLD, 20));
-		lblNewLabel_9.setBounds(94, 342, 345, 62);
-		panel.add(lblNewLabel_9);
+		lblNewLabel_9.setBounds(30, 243, 400, 26);
+		lblNewLabel_9.setFont(new Font("Poppins", Font.BOLD, 20));
+		lblNewLabel_9.setForeground(new Color(0, 0, 0));
+		layeredPane.add(lblNewLabel_9, Integer.valueOf(1));
 	}
 }
